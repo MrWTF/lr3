41cfffb4386e53e2b6bed7d8f5e5ac3af2be7ac2
Kondrashin <uzbagoysya.666@yandex.ru>
2014-10-24 12:06:32 +0400

    division

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 //Учебная программа
 int main ()
 {
-   double x,y,sum,razn;
+   double x,y,sum,razn,div;
    cout<<"Enter 2 numbers \n";
    cin>>x;
    cin>>y;
@@ -10,4 +10,6 @@ int main ()
    cout<<"Sum = "<<sum<<"\n";
    razn=x-y;
    cout<<"Difference = "<<razn<<"\n";
+   div=x/y;
+   cout<<"Division = "<<div<<"\n";
 }
