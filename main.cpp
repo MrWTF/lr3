8eca4d9d6296092b11db65b6c7cd4a01adcb6d8b
Kondrashin <uzbagoysya.666@yandex.ru>
2014-10-24 11:01:44 +0400

    With difference function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "sdt.h"
 int main ()
 {
-   double x,y,sum;
+   double x,y,sum,razn;
    cout<<"Enter 2 numbers \n";
    cin>>x;
    cin>>y;
    sum=x+y;
    cout<<"Sum = "<<sum<<"\n";
+   razn=x-y;
+   cout<<"Difference = "<<razn<<"\n";
 }
